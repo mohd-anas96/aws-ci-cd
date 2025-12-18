c7bd4b5a28c563a5c3d66e3ae4e2027b17e8814b
mohd-anas96 <mohd.anas4@globallogic.com>
2025-12-18 13:53:48 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Find running container ID (if any)
+container_id=$(docker ps -q)
+
+# Stop and remove the container if it exists
+if [ -n "$container_id" ]; then
+  docker rm -f "$container_id"
+else
+  echo "No running container found"
+fi
